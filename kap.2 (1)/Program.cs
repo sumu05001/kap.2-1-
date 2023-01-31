3d5d7d48dd1544040b44451955ab77658f118d57
Tsnem Musa <tm222qf@student.lnu.se>
2023-01-31 13:55:56 +0100

    Add project files.

---
+++ kap.2 (1)/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace uppgift1
+{
+    class program
+    {
+    static void Main(string[] args)
+        {
+        Console.WriteLine("Vilken ör den favoritbok?");
+        string bok = Console.ReadLine();
+        Console.WriteLine("Din favoritbok är "  +  bok );
+
+        }
+    }
+}
